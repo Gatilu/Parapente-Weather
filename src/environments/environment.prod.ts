31adc824f77bbbd6e899e9f94d2b7f18999b3250
Tiago <gabrieltiagolucas62@gmail.com>
2024-06-11 02:16:24 -0300

    app final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAhBkMl8b3dWRFa97SDKP3LJIswolFkW7A",
+    authDomain: "parapente-weather.firebaseapp.com",
+    projectId: "parapente-weather",
+    storageBucket: "parapente-weather.appspot.com",
+    messagingSenderId: "1064762284974",
+    appId: "1:1064762284974:web:d03f8f04e9a17f9cb9c63f",
+    measurementId: "G-KDZWCQNJTF"
+  }
 };
